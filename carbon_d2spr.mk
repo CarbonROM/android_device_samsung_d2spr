a52e22a3d970c95d43aa63154349b0c2b86d774c
Joe Maples <joe@frap129.org>
2017-05-16 16:47:55 -0500

    d2spr: Carbonize
    Signed-off-by: Joe Maples <joe@frap129.org>

--- carbon_d2spr.mk
+++ carbon_d2spr.mk
@@ -1,13 +1,15 @@
 $(call inherit-product, device/samsung/d2spr/full_d2spr.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Carbon stuff.
+$(call inherit-product, vendor/carbon/config/common.mk)
+$(call inherit-product, vendor/carbon/config/cdma.mk)
+
+PRODUCT_NAME := carbon_d2spr
+PRODUCT_DEVICE := d2spr
+PRODUCT_MANUFACTURER := LGE
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=d2spr \
     TARGET_DEVICE=d2spr \
     BUILD_FINGERPRINT="samsung/d2spr/d2spr:4.4.2/KOT49H/L710VPUDNJ2:user/release-keys" \
     PRIVATE_BUILD_DESC="d2spr-user 4.4.2 KOT49H L710VPUDNJ2 release-keys"
-
-PRODUCT_NAME := lineage_d2spr
-PRODUCT_DEVICE := d2spr
